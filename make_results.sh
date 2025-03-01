e27590f39f2331e2c15f3e26e0fdabc2837d9e36
Toori <bzang@gmx.de>
2025-03-01 18:11:16 +0100

    Minor adjustment to bash which actually creates branch coverage

--- make_results.sh
+++ make_results.sh
@@ -2,7 +2,7 @@ python3 gcov.py -p output/gcov_output.bin
 mkdir -p results
 
 lcov --branch-coverage --mcdc-coverage --capture --directory . --output-file results/coverage.info
-lcov --remove results/coverage.info -o results/filtered_coverage.info \
+lcov --branch-coverage --mcdc-coverage --remove results/coverage.info -o results/filtered_coverage.info \
             '/usr/include/c++/*' '/doctest/*'
 genhtml --branch-coverage results/filtered_coverage.info --output-directory results/html
 
